5fc3c5dcc20608c6a6e3b506776bed9855d6370b
Eduard Wade Lerman <eduardsnaga@gmail.com>
2022-09-21 00:02:14 +0800

    pangkat belum bisa

--- Tugas 1/pangkat.py
+++ Tugas 1/pangkat.py
@@ -1,4 +1,12 @@
-def pangkat(x, y):
-    print(sum(x*x for x in range(y)))
+from unittest import result
 
-pangkat(3,2)
+
+def pangkat(x, y):
+    for x in range(1, y+1):
+        result = result*x
+    return result
+
+pangkat(3,5)
+
+print(3*3)
+print(sum(3*3 for x in range(1)))
